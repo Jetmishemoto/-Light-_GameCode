9f20f898bb970492489e39da45a9caa33682b6ac
Jetmishemoto <Daadoniwp92@gmx.com>
2022-02-22 06:02:32 -0500

    Add files via upload

--- AnimationStateController.cs
+++ AnimationStateController.cs
@@ -4,40 +4,45 @@ using UnityEngine;
 
 public class AnimationStateController : MonoBehaviour
 {
-
-    Animator _anim;
-
-
-
-      private AnimatorStateInfo currentStateInfo_0;
+    private Playa playa;
+    private Animator _anim;
+    private AnimatorStateInfo currentStateInfo_0;
 
 
-    
+
+
+    void Awake()
+    {
+        playa = GetComponent<Playa>();
+        _anim = GetComponent<Animator>();
+
+    }
 
 
     public void SwordAttackStateMonitor()
     {
         if (currentStateInfo_0.IsName(""))
         {
+           //playa._canMove = false;
 
+        }
+        if (playa._canMove)
+        {
+
+            
+            print("CanMove");
         }
 
 
-
     }
 
 
    
 
-    void Awake()
-    {
-   
-
-    }
 
     void Update()
     {
-
+        SwordAttackStateMonitor();
         currentStateInfo_0 = _anim.GetCurrentAnimatorStateInfo(0);
 
     }
